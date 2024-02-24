c5c30115e5dade05b712cc8308e851da92fbc86d
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-02-24 13:30:40 +0100

    commit

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://GULLIET:5yEBvpGO505z2u7G@cluster0.xbbymqv.mongodb.net/Locapic';
+const connectionString = process.env.CONNECTION_STRING;
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
     .then(() => console.log('Database connected'))
     .catch(error => console.error(error));
